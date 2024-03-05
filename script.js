fe9444b54107c63122d152b7c9ced28a4a31b474
Christian Esparza <cesparzajp@gmail.com>
2024-03-05 21:14:07 +0900

    Display canvas and add cells

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function generateCanvas(){
     canvas.style.display='flex';
-    
+    canvas.innerHTML = '';   //clear the canvas
+    for(let i=0; i < numberOfCells; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        canvas.appendChild(cell);
+    } 
+    // height.addEventListener('input', () => canvas.style.height = `${height.value*cellSize}px`);
+}
+
+function setWidth(){
+
 }
 
 function showInstructions(){
@@ -23,4 +33,11 @@ instructions.addEventListener('click',showInstructions);
 const startBtn=document.querySelector('#start');
 startBtn.addEventListener('click',generateCanvas);
 
-const canvas=document.querySelector('.canvas');
+const size=document.querySelector('#size');
+let numberOfCells = 100;  //Default size 10 X 10 cells
+size.addEventListener('input', () => {numberOfCells=parseInt(size.value)**2;});
+// const height=document.querySelector('#height');
+const brightness=document.querySelector('#brightness');
+// size.addEventListener('input', () => canvas.style.width = `${width.value*cellSize}px`);
+
+const canvas=document.querySelector('#canvas');
